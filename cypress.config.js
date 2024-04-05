47c58a00e283db979beb9f3c3f3f64644d24e581
semous2006 <151662132+semous2006@users.noreply.github.com>
2024-04-04 22:29:12 -0400

    first commet

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '5bo2zu',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
