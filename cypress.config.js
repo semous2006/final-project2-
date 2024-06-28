8a79e7691bc81f173de32e2ab8f9066e85756418
Eslam Hassan <151662132+semous2006@users.noreply.github.com>
2024-06-28 00:09:46 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-<<<<<<< HEAD
   projectId: '5bo2zu',
-=======
->>>>>>> b0a4523763cdbe23761d078577e1a44dec941372
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
-<<<<<<< HEAD
-{
-  "name": "final-project2",
-  "version": "1.0.0",
-  "description": "",
-  "main": "cypress.config.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "cy:run": "cypress run"
-  },
-  "keywords": [],
-  "author": "Eslam Hassan",
-  "license": "ISC"
-}
-=======
->>>>>>> b0a4523763cdbe23761d078577e1a44dec941372
+
+
+
