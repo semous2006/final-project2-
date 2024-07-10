2b875f8884b6031aaeccb48fe3cef0ee78098994
semous2006 <151662132+semous2006@users.noreply.github.com>
2024-07-10 12:25:47 -0400

    Your commit message here

---
+++ cypress/e2e/support/commands.js
@@ -0,0 +1,8 @@
+// cypress/support/commands.js
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('/login');
+    cy.get('#email').type(email);
+    cy.get('#password').type(password);
+    cy.get('button[type="submit"]').click();
+});
